aaf28ca2409f323f2d9a3e2165f63fcc8b959096
Mateo Sheshi <mateo.sheshi@stud.tecnicosuperiorekennedy.it>
2022-06-29 19:50:01 +0200

    feat : added get user secure contacts

--- src/modules/userSecureContacts.js
+++ src/modules/userSecureContacts.js
@@ -1,1 +1,11 @@
+const { constants } = require('../config');
+
 // TODO : implement module to get secure contacts from RDS by username
+const getSecureContactsByUsername = async (username) => {
+    // GET /user-secure-contacts , parameters : {username}
+    const response = await fetch(constants.API_GATEWAY + `/user-secure-contacts/?username=${username}`);
+    const result = await response.json();
+    return result.secureContacts;
+}
+
+module.exports = getSecureContacts;
