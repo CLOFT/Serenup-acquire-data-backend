378c395b92e1916743d960e9d9dd82f127bc6db8
Mateo Sheshi <mateo.sheshi@stud.tecnicosuperiorekennedy.it>
2022-07-01 15:25:12 +0200

    feat : send and save alarms done

--- src/index.js
+++ src/index.js
@@ -6,23 +6,3 @@ const processData  = require('./modules/processData');
 exports.handler = async (event, context) => {
   if (event.body != null) await processData(event.body);
 };
-
-/*
-
-PAYLOAD Bracelets
-{
-  "braceletId" : "guid",
-  "heartbeat" : "int",
-  "actualPosition" : "varchar (coordinates  example : 45.962650,  12.655040)",
-  "bloodPressure" : {
-    "systolicPressure" : "int",
-    "diastolicPressure" : "int"
-  },
-  "oxygenSaturation" : "int (0 - 100%)",
-  "steps" : "int (0 - n)",
-  "battery" : "int (0 - 100 %)",
-  "timestamp" : "timestamp",
-  "alarm" : "FALL || LOW_BATTERY"
-}
-
-*/
