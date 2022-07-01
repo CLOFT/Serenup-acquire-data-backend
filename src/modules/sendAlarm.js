4ef19572236028aeb3f9c04dfe9bba6f557e77a1
Mateo Sheshi <mateo.sheshi@stud.tecnicosuperiorekennedy.it>
2022-07-01 10:20:06 +0200

    chore : added send alarm mail with SNS

--- src/modules/sendAlarm.js
+++ src/modules/sendAlarm.js
@@ -13,96 +13,31 @@ const extractContactsEmails = async (secureContacts) =>
 
 // Send alarm message with SES
 const sendAlarm = async (body) => {
-  const username = await getUsernameByBraceletsId(body.braceletId);
+  try {
+    const username = await getUsernameByBraceletsId(body.braceletId);
 
-  const secureContacts = await getSecureContactsByUsername(username);
+    const secureContacts = await getSecureContactsByUsername(username);
 
-  const link = constants.MAPS_SEARCH_LINK + encodeURIComponent(body.position);
+    const link = constants.MAPS_SEARCH_LINK + encodeURIComponent(body.position);
+    const message = `
+      Alarm of Fall! You're a secure contact of ${username} 
+      Tap here to see your friend's position --> ${link}
+      `;
 
-  const message = `
-    Alarm of Fall! You're a secure contact of ${username} </p>
-    His/Her current position is : ${body.position}</p>
-    Tap here to see --> ${link}
-    `;
-  /*
-  alarm payload 
-  {
-    BraceletId: body.BraceletId,
-      Type: body.Alarm,
-      Value: true,
-      Position : body.position,
-      SecureContacts: secureContacts,
-      // TODO : review payload for alarms again
-      // TODO : check payload format of secureContacts
+    // prepare params for SNS message
+
+    var params = {
+      Message: message,
+      Subject: 'Fall Alarm',
+      TopicArn: constants.TOPIC_ARN,
+    };
+
+    // Publish message
+
+    const res = await sns.publish(params).promise();
+  } catch (error) {
+    console.log(error);
   }
-  */
-  // TODO : send alarm message with SES
-
-  // Create sendEmail params
-  var bccAddresses = await extractContactsEmails(secureContacts);
-
-  var params = {
-    Destination: {
-      /* required */
-      BccAddresses: bccAddresses,
-    },
-    Message: {
-      /* required */
-      Body: {
-        /* required */
-        Html: {
-          Charset: 'UTF-8',
-          Data: message,
-        },
-        // Text: {
-        //   Charset: 'UTF-8',
-        //   Data: 'TEXT_FORMAT_BODY',
-        // },
-      },
-      Subject: {
-        Charset: 'UTF-8',
-        Data: 'Alarm email',
-      },
-    },
-    Source: constants.SENDER_EMAIL_ADDRESS /* required */,
-    // ReplyToAddresses: [
-    //   'EMAIL_ADDRESS',
-    //   /* more items */
-    // ],
-  };
-
-  const res = await ses.sendEmail(params).promise();
-  let a = 5;
-
-  /* body : 
-        {
-            BraceletId : "guid",
-            Type : "FALL || LOW_BATTERY",
-            Value : "int" (only for Battery at the moment),
-            SecureContacts : [  // array with contacts' email
-                {
-                    email : "email",
-                },
-                ...
-            ]
-        }   
-    */
 };
-
-// TODO : create a mock test to send alarm from local
-const sendAlarmTest = async () => {
-  // TODO : implement send SNS msg test
-  const body = {};
-  await sendAlarm({
-    braceletId: '8e8a89d3-bec2-47ba-8669-437970a6f3f5',
-    type: 'LOW_BATTERY',
-    position: '45.962650,12.655040',
-    value: 18,
-  });
-};
-
-(async () => {
-  await sendAlarmTest();
-})();
 
 module.exports = sendAlarm;
